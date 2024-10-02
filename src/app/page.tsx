1aa4ae13674cfac084249909fba84170f30f4160
Henrique Izzi <roqueehs@gmail.com>
2024-10-01 23:04:19 -0300

    ajustepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/calculo"></Link>
-      <Link href="/ideia1"></Link>
-      <Link href="/ideia2"></Link>
-      <Link href="/ideia3"></Link>
-
-      
     </div>
   );
 }
